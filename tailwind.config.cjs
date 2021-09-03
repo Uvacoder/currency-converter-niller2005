3a9a268fedbc9230dfa2513c90fabe38bbed4f0f
Jacob Nielsen <jnillern@gmail.com>
2021-09-03 08:32:12 +0200

    Svelte + Vite init

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{svelte,js,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['dark'],
+  },
+};
